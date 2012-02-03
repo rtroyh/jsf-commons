64738f7e4864da43a3299eab838fcb205f4451cf
rtroy <rtroy@13f10598-5c34-4a87-bf25-6f6a81c8df7a>
2012-02-03 19:54:31 +0000

    REFORMAT CODE
    git-svn-id: https://gathercl.svn.cloudforge.com/java/maven/jsfcommons@7426 13f10598-5c34-4a87-bf25-6f6a81c8df7a

--- src/main/java/com/gather/jsfcommons/view/managedbeans/ErrorBean.java
+++ src/main/java/com/gather/jsfcommons/view/managedbeans/ErrorBean.java
@@ -9,50 +9,51 @@ import javax.servlet.ServletException;
 
 public class ErrorBean {
 
-	public String getStackTrace() {
-		FacesContext context = FacesContext.getCurrentInstance();
-		Map<String, Object> request = context.getExternalContext().getRequestMap();
+    public String getStackTrace() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        Map<String, Object> request = context.getExternalContext().getRequestMap();
 
-		Throwable ex = (Throwable) request.get("javax.servlet.error.exception");
-		StringWriter sw = new StringWriter();
-		PrintWriter pw = new PrintWriter(sw);
-		fillStackTrace(	ex,
-						pw);
+        Throwable ex = (Throwable) request.get("javax.servlet.error.exception");
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        fillStackTrace(ex,
+                       pw);
 
-		return sw.toString();
-	}
+        return sw.toString();
+    }
 
-	private static void fillStackTrace(	Throwable t,
-										PrintWriter w) {
-		if (t == null)
-			return;
+    private static void fillStackTrace(Throwable t,
+                                       PrintWriter w) {
+        if (t == null) {
+            return;
+        }
 
-		t.printStackTrace(w);
+        t.printStackTrace(w);
 
-		if (t instanceof ServletException) {
-			Throwable cause = ((ServletException) t).getRootCause();
+        if (t instanceof ServletException) {
+            Throwable cause = ((ServletException) t).getRootCause();
 
-			if (cause != null) {
-				w.println("Causa raíz: ");
-				fillStackTrace(	cause,
-								w);
-			}
-		} else if (t instanceof SQLException) {
-			Throwable cause = ((SQLException) t).getNextException();
+            if (cause != null) {
+                w.println("Causa raíz: ");
+                fillStackTrace(cause,
+                               w);
+            }
+        } else if (t instanceof SQLException) {
+            Throwable cause = ((SQLException) t).getNextException();
 
-			if (cause != null) {
-				w.println("Excepción sgte: ");
-				fillStackTrace(	cause,
-								w);
-			}
-		} else {
-			Throwable cause = t.getCause();
+            if (cause != null) {
+                w.println("Excepción sgte: ");
+                fillStackTrace(cause,
+                               w);
+            }
+        } else {
+            Throwable cause = t.getCause();
 
-			if (cause != null) {
-				w.println("Cause:");
-				fillStackTrace(	cause,
-								w);
-			}
-		}
-	}
+            if (cause != null) {
+                w.println("Cause:");
+                fillStackTrace(cause,
+                               w);
+            }
+        }
+    }
 }
