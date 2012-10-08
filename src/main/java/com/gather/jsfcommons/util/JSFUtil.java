1b639c51567f36293bb697bdf75efc3ee4a9c80e
rtroy <rtroy@13f10598-5c34-4a87-bf25-6f6a81c8df7a>
2012-10-08 14:27:17 +0000

    MAVEN REFACTORING
    git-svn-id: https://gathercl.svn.cloudforge.com/java/maven/jsfcommons@8164 13f10598-5c34-4a87-bf25-6f6a81c8df7a

--- src/main/java/com/gather/jsfcommons/util/JSFUtil.java
+++ src/main/java/com/gather/jsfcommons/util/JSFUtil.java
@@ -2,15 +2,20 @@ package com.gather.jsfcommons.util;
 
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
+import java.util.Map;
 
 public final class JSFUtil {
 
-    public static String getDirectoryPath() {
-        FacesContext aFacesContext = FacesContext.getCurrentInstance();
-        ServletContext context = (ServletContext) aFacesContext.getExternalContext().getContext();
-        String rootpath = context.getRealPath("/");
+    public static String getDirectoryPath(FacesContext fc) {
+        ServletContext context = (ServletContext) fc.getExternalContext().getContext();
 
-        return rootpath;
+        return context.getRealPath("/");
+    }
+
+    public static String getParameterValue(FacesContext fc,
+                                           String parameterName) {
+        Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+        return params.get("evaluacion");
     }
 
 }
