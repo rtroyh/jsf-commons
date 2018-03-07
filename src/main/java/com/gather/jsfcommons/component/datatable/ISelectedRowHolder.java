94530023d54f8ad6d1982d233157c1ee4a4600a0
rtroy <rtroyh@gather.cl>
2018-03-07 12:48:46 -0300

    -Se quitaron clases en desuso -Actualizacion a JSF 2.3

--- src/main/java/com/gather/jsfcommons/component/datatable/ISelectedRowHolder.java
+++
@@ -1,8 +0,0 @@
-package com.gather.jsfcommons.component.datatable;
-
-public interface ISelectedRowHolder<E extends Row> {
-
-    public void setSelectedRow(E selectedRow);
-
-    public E getSelectedRow();
-}
